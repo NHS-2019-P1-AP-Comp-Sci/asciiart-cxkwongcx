3c548d190cc312d099b84819dc14c9ef427eece5
s-kwonge <s-kwonge@53S50022879.it.bsd405.org>
2019-09-20 08:08:33 -0700

    Turning in project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Unit 1 Project
  * 
  * In this project you have the opportunity to create a piece of artwork using ASCII 
@@ -17,11 +17,74 @@
 // YOU CODE HERE :) 
 
 public class ASCIIArt {
-	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
+	public static void heading() {
+		System.out.println("                               ------------");
+		System.out.println("                               AP CS Rocks!");
+		System.out.println("                               ------------");
+	}
+
+	public static void topHead() {
+		System.out.print("                                ________     \n"
+				+ "                               /(.)  (.)\\    \n"
+				+ "                              /     u    \\   \n" + "                             |      _     |  \n"
+				+ "                             |     / \\    |  \n");
+		fingersAndBottomHead();
+	}
+
+	public static void fingersAndBottomHead() {
+		System.out.print("              nnnnn           \\    \\_/   /           nnnnn\n"
+				+ "             | | | |n          \\________/          n| | | |\n");
+	}
+
+	public static void armsAndBody() {
+		System.out.print("            |       |           |      |           |       |\n"
+				+ "             \\_   _/           /        \\           \\_   _/\n"
+				+ "              \\   \\    ___    /     A    \\    ___    /   /\n"
+				+ "               \\   \\__/   \\__|     /|\\    |__/   \\__/   /\n"
+				+ "                \\            |    / | \\   |            /\n"
+				+ "                 \\___________|      |     |___________/\n"
+				+ "                             |      |     |  \n" + "                             |      |     |  \n"
+				+ "                             |      |     | \n"
+				+ "                             |____________|  \n");
+	}
+	
+	public static void legs() {
+		System.out.print("                              \\   /  \\   /   \n"
+				+ "                               | |    | |    \n" + "                               | |    | |    \n"
+				+ "         ___     ___           | |    | |             ___     ___\n" + "        |   |___|   |          | |    | |            |   |___|   |\n"
+				+ "        | 4 |___| 0 |          | |    | |            | 4 |___| 0 | \n");
+
+	}
+
+	public static void shoes() {
+		System.out.println(
+				"        |___|   |___|          | |_   | |_           |___|   |___| \n" + "                               {___)  {___)  ");
+
+	}
+
+	public static void description() {
+		System.out.println("\t\tThis is a pixel man!^\t(Who skipped leg day)");
+		// System.out.println("\"This is my output.\"");//
+	}
+
+	public static void main(String[] args) {
+		heading();
+		topHead();
+		armsAndBody();
+		legs();
+		shoes();
+		description();
+
+		/*
+		 * 
+		 * 
+		 * System.out.println(" ");
+		 * 
+		 * System.out.println("THIS IS MY ASCII ART!!!!");
+		 * System.out.println("THIS IS MY ASCII ART!!!!");
+		 * System.out.println("THIS IS MY ASCII ART!!!!");
+		 * System.out.println("THIS IS MY ASCII ART!!!!");
+		 * System.out.println("THIS IS MY ASCII ART!!!!");
+		 */
 	}
 }
